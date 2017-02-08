c6a3edc54157d736e85783e71a0557f66780e5cf
Jonas <jonas.kemper.biz@gmail.com>
2017-02-08 16:14:20 +0100

    intermediate state

--- src/main/scala/io/github/jonasrk/App.scala
+++ src/main/scala/io/github/jonasrk/App.scala
@@ -1,48 +1,81 @@
 package io.github.jonasrk
 import org.qcri.rheem.api._
 import org.qcri.rheem.core.api.{Configuration, RheemContext}
+import org.qcri.rheem.core.function.FunctionDescriptor.ExtendedSerializableFunction
 import org.qcri.rheem.java.Java
 import org.qcri.rheem.spark.Spark
 
-/**
-  * @author ${user.name}
-  */
+import scala.util.Random
+import scala.collection.JavaConversions._
+import org.qcri.rheem.core.function.ExecutionContext
+import org.qcri.rheem.core.optimizer.costs.LoadProfileEstimators
+
 object App {
 
   def main(args : Array[String]): Unit = {
 
     // Settings
     val inputUrl = "file:/Users/jonas/tmp.txt"
+    val iterations = 20
+    val n = 5
 
     // Get a plan builder.
     val rheemContext = new RheemContext(new Configuration)
       .withPlugin(Java.basicPlugin)
       .withPlugin(Spark.basicPlugin)
     val planBuilder = new PlanBuilder(rheemContext)
-      .withJobName(s"WordCount ($inputUrl)")
+      .withJobName(s"k-means ($inputUrl, $iterations iterations)")
       .withUdfJarsOf(this.getClass)
 
-    val wordcounts = planBuilder
-      // Read the text file.
-      .readTextFile(inputUrl).withName("Load file")
+    // Read and parse the input file(s).
+    val points = planBuilder
+      .readTextFile(inputUrl).withName("Read file")
+      .map { line =>
+        val fields = line.split(",")
+        TaggedPoint(fields(0).toDouble, fields(1).toDouble, -1)
+      }.withName("Create points")
 
-      // Split each line by non-word characters.
-      .flatMap(_.split("\\W+"), selectivity = 10).withName("Split words")
+    case class Point(x: Double, y: Double)
+    case class TaggedPoint(x: Double, y: Double, cluster: Int)
+    case class TaggedPointCounter(x: Double, y: Double, cluster: Int, count: Long) {
+      def +(that: TaggedPointCounter) = TaggedPointCounter(this.x + that.x, this.y + that.y, this.cluster, this.count + that.count)
+      def average = TaggedPoint(x / count, y / count, cluster)
+    }
 
-      // Filter empty tokens.
-      .filter(_.nonEmpty, selectivity = 0.99).withName("Filter empty words")
+    // Create initial centroids.
+    val random = new Random
+    val initialCentroids = planBuilder
+      .loadCollection(for (i <- 1 to n) yield TaggedPoint(random.nextGaussian(), random.nextGaussian(), i)).withName("Load random centroids")
 
-      // Attach counter to each word.
-      .map(word => (word.toLowerCase, 1)).withName("To lower case, add counter")
+    // Declare UDF to select centroid for each data point.
+    class SelectNearestCentroid extends ExtendedSerializableFunction[Point, TaggedPoint] {
 
-      // Sum up counters for every word.
-      .reduceByKey(_._1, (c1, c2) => (c1._1, c1._2 + c2._2)).withName("Add counters")
-      .withCardinalityEstimator((in: Long) => math.round(in * 0.01))
+      /** Keeps the broadcasted centroids. */
+      var centroids: Iterable[TaggedPoint] = _
 
-      // Execute the plan and collect the results.
+      override def open(executionCtx: ExecutionContext) = {
+        centroids = executionCtx.getBroadcast[TaggedPoint]("centroids")
+      }
+
+      override def apply(point: Point): TaggedPointCounter = {
+        var minDistance = Double.PositiveInfinity
+        var nearestCentroidId = -1
+        for (centroid <- centroids) {
+          val distance = Math.pow(Math.pow(point.x - centroid.x, 2) + Math.pow(point.y - centroid.y, 2), 0.5)
+          if (distance < minDistance) {
+            minDistance = distance
+            nearestCentroidId = centroid.cluster
+          }
+        }
+        new TaggedPointCounter(point.x, point.y, nearestCentroidId, 1)
+      }
+    }
+
+    // Do the k-means loop.
+    val finalCentroids = initialCentroids.repeat(iterations, { initialCentroids => points}).withName("Loop")
+
+      // Collect the results.
       .collect()
-
-    println(wordcounts)
 
   }
 
